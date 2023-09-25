04d0d1b98ca21a1daf24ae839c0124f5487aaf2d
Joseph <u20211c828@upc.edu.pe>
2023-09-25 13:38:43 -0500

    first commit

---
+++ src/main/java/com/unieventia/unieventia_api/UnieventiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.unieventia.unieventia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnieventiaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnieventiaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unieventia/unieventia_api/UnieventiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unieventia.unieventia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnieventiaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
